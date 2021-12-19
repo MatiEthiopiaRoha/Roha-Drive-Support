25013590383ce4c8101f95d7728c41a55d6286b8
Mati Ethiopia <91181733+MatiEthiopiaRoha@users.noreply.github.com>
2021-12-19 22:33:38 +0000

    Add files via upload

---
+++ Roha Drive Support/Roha Drive Support/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Roha_Drive_Support
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startsys = new System.Windows.Forms.Button();
+            this.stopsys = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(632, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "ርቀት";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(710, 547);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(12, 121);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(614, 364);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COM PORT";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "921600"});
+            this.comboBox2.Location = new System.Drawing.Point(181, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BUAD RATE";
+            // 
+            // startsys
+            // 
+            this.startsys.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startsys.Location = new System.Drawing.Point(325, 30);
+            this.startsys.Name = "startsys";
+            this.startsys.Size = new System.Drawing.Size(121, 23);
+            this.startsys.TabIndex = 6;
+            this.startsys.Text = "ሲስተም ጀምር";
+            this.startsys.UseVisualStyleBackColor = true;
+            this.startsys.Click += new System.EventHandler(this.startsys_Click);
+            // 
+            // stopsys
+            // 
+            this.stopsys.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopsys.Location = new System.Drawing.Point(325, 60);
+            this.stopsys.Name = "stopsys";
+            this.stopsys.Size = new System.Drawing.Size(121, 23);
+            this.stopsys.TabIndex = 7;
+            this.stopsys.Text = "ሲስተም አቁም";
+            this.stopsys.UseVisualStyleBackColor = true;
+            this.stopsys.Click += new System.EventHandler(this.stopsys_Click);
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.status.ForeColor = System.Drawing.Color.Red;
+            this.status.Location = new System.Drawing.Point(12, 491);
+            this.status.Multiline = true;
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(405, 68);
+            this.status.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 60);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(290, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ሮሃ የተሽከርካሪ ቴክኖሎጂ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 564);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.stopsys);
+            this.Controls.Add(this.startsys);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Roha Drive Support";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startsys;
+        private System.Windows.Forms.Button stopsys;
+        private System.Windows.Forms.TextBox status;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Roha Drive Support/Roha Drive Support/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using rtChart;
+
+namespace Roha_Drive_Support
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            stopsys.Enabled = false;
+            getports();
+        }
+        kayChart serialDataChart;
+        private DateTime datetime;
+        void getports()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        private void startsys_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "")
+                {
+                    label3.Text = "እባክዎ ፖርት መሙያውን በትክክል ይሙሉ";
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.StopBits = StopBits.One;
+                    label3.Text = "ሲስትሙ ተጀምራል";
+                    progressBar1.Value = 100;
+                    startsys.Enabled = false;
+                    stopsys.Enabled = true;
+                    serialPort1.Open();
+                    serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceivedEventHandler);
+                    if (!serialPort1.IsOpen)
+                    {
+                        serialPort1.Open();
+                    }
+                }
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+                label3.Text = "ያልተፈቀደ ትእዛዝ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialDataChart = new kayChart(chart1, 60);
+
+            serialDataChart.serieName = "ርቀት";
+        }
+        private void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            datetime = DateTime.Now;
+            SerialPort output = sender as SerialPort;
+            string input = output.ReadLine();
+
+            textBox1.Invoke((MethodInvoker)delegate { textBox1.AppendText(datetime + "\t" + "LIDAR Distance" + "\t" + input + "\n"); });
+            // initiaization of chart
+            double data;
+            bool result = Double.TryParse(input, out data);
+            if (result)
+            {
+                serialDataChart.TriggeredUpdate(data);
+            }
+        }
+
+        private void stopsys_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            label3.Text = "ሲስተሙ ተዘግቷል";
+            progressBar1.Value = 0;
+            startsys.Enabled = true;
+            stopsys.Enabled = false;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
